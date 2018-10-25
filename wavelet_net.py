24dd93f5ae97d0462702350d8412de680550fc30
Rohan-o-Tammara <ta.rohan550@gmail.com>
2018-10-25 15:46:52 +0530

    wavelet_transform commented

--- wavelet_net.py
+++ wavelet_net.py
@@ -4,7 +4,7 @@ import pywt
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Input, Dense, Dropout
-from keras.optimizers import Adam
+from keras.optimizers import Adam, SGD
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 from keras.utils import to_categorical
 """
@@ -32,6 +32,7 @@ def wavelet_transform(wave, wavelet, level):
 def prepare_data():
     # Load data
     M = 720000
+    O = int(M/4)
     N = 900000
 
     print('Fetching data')
@@ -41,51 +42,96 @@ def prepare_data():
     df_test = pd.concat([df[M:N], df[N+M:2*N], df[2*N+M:3*N], df[3*N+M:4*N]])
 
     x_train = df_train.iloc[:, :-1].values
-    y_train = df_train.iloc[:, 3].values
     x_test = df_test.iloc[:, :-1].values
-    y_test = df_test.iloc[:, 3].values
+    y_train = to_categorical(df_train['y'].values)
+    y_test = to_categorical(df_test['y'].values)
+    #y_train = to_categorical(np.repeat([[0, 1, 2, 3]], M, axis=0).flatten())
+    #y_test = to_categorical(np.repeat([[0, 1, 2, 3]], O, axis=0).flatten())
     print('done')
 
     del [df, df_train, df_test]
 
     # Preprocessing
-
+    '''
     print('Performing wavelet transforms')
-    i1_train = wavelet_transform(x_train[:, 0], wavelet='db4', level=L)
-    i2_train = wavelet_transform(x_train[:, 1], wavelet='db4', level=L)
-    i3_train = wavelet_transform(x_train[:, 2], wavelet='db4', level=L)
-    i1_test = wavelet_transform(x_test[:, 0], wavelet='db4', level=L)
-    i2_test = wavelet_transform(x_test[:, 1], wavelet='db4', level=L)
-    i3_test = wavelet_transform(x_test[:, 2], wavelet='db4', level=L)
+    wvlt = 'db13'
+    i1_train = [wavelet_transform(x_train[:M, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[M:2*M, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[2*M:3*M, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[3*M:, 0], wavelet=wvlt, level=L)]
+
+    i2_train = [wavelet_transform(x_train[:M, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[M:2*M, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[2*M:3*M, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[3*M:, 1], wavelet=wvlt, level=L)]
+
+    i3_train = [wavelet_transform(x_train[:M, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[M:2*M, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[2*M:3*M, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_train[3*M:, 2], wavelet=wvlt, level=L)]
+
+    i1_test = [wavelet_transform(x_test[:O, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[O:2*O, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[2*O:3*O, 0], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[3*O:, 0], wavelet=wvlt, level=L)]
+
+    i2_test = [wavelet_transform(x_test[:O, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[O:2*O, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[2*O:3*O, 1], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[3*O:, 1], wavelet=wvlt, level=L)]
+
+    i3_test = [wavelet_transform(x_test[:O, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[O:2*O, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[2*O:3*O, 2], wavelet=wvlt, level=L),
+                wavelet_transform(x_test[3*O:, 2], wavelet=wvlt, level=L)]
     print('done')
+
+    """ # Plots
+    plt.subplot(4,1,1)
+    plt.plot(x_train[2160000:2165000, 0])
+    plt.subplot(4,1,2)
+    plt.plot(i1_train[1])
+    plt.subplot(4,1,3)
+    plt.plot(i2_train[1])
+    plt.subplot(4,1,4)
+    plt.plot(i3_train[1])
+    plt.show()
+    """
 
     del [x_train, x_test]
 
     print('Preparing training data')
-    x_train = np.concatenate((i1_train, i2_train, i3_train), axis=0)
+    x_train = np.concatenate((np.concatenate((i1_train[0], i2_train[0], i3_train[0]), axis=0),
+                            np.concatenate((i1_train[1], i2_train[1], i3_train[1]), axis=0),
+                            np.concatenate((i1_train[2], i2_train[2], i3_train[2]), axis=0),
+                            np.concatenate((i1_train[3], i2_train[3], i3_train[3]), axis=0)), axis=1)
+
     x_train = np.transpose(x_train)
-    y_train = to_categorical(y_train)
     print('done')
 
     print('Preparing test data')
-    x_test = np.concatenate((i1_test, i2_test, i3_test), axis=0)
+    x_test = np.concatenate((np.concatenate((i1_test[0], i2_test[0], i3_test[0]), axis=0),
+                            np.concatenate((i1_test[1], i2_test[1], i3_test[1]), axis=0),
+                            np.concatenate((i1_test[2], i2_test[2], i3_test[2]), axis=0),
+                            np.concatenate((i1_test[3], i2_test[3], i3_test[3]), axis=0)), axis=1)
+
     x_test = np.transpose(x_test)
-    y_test = to_categorical(y_test)
     print('done')
 
-    del [i1_train, i1_test, i2_train, i2_test, i3_train, i3_test]
-
+    del [i1_test, i2_test, i3_test, i1_train, i2_train, i3_train]
+    '''
     return x_train, y_train, x_test, y_test
 
 def network(input_shape):
-    # Network Arch.
+    # Network Arch. (Use RNN maybe)
     net = Sequential()
-    net.add(Dense(64, input_shape=input_shape, kernel_initializer='lecun_uniform', activation='relu'))
-    net.add(Dropout(0.2))
-    net.add(Dense(128, kernel_initializer='lecun_uniform', activation='relu'))
-    net.add(Dense(128, kernel_initializer='lecun_uniform', activation='relu'))
-    net.add(Dropout(0.4))
+    net.add(Dense(128, input_shape=input_shape, kernel_initializer='lecun_uniform', activation='relu'))
     net.add(Dense(256, kernel_initializer='lecun_uniform', activation='relu'))
+    net.add(Dropout(0.2))
+    net.add(Dense(256, kernel_initializer='lecun_uniform', activation='relu'))
+    net.add(Dense(512, kernel_initializer='lecun_uniform', activation='relu'))
+    net.add(Dropout(0.4))
+    net.add(Dense(512, kernel_initializer='lecun_uniform', activation='relu'))
     net.add(Dense(4, kernel_initializer='lecun_uniform', activation='softmax'))
 
     optimizer = Adam(lr=0.001)
@@ -96,13 +142,13 @@ def network(input_shape):
 
 def train(x_train, y_train, x_test, y_test):
     # Training
-    NUM_EPOCHS = 50
+    NUM_EPOCHS = 10
     BATCH_SIZE = 1024
 
-    reduce_lr = ReduceLROnPlateau(monitor='acc', factor=0.1, patience=3, min_lr=0.0001)
+    reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=3, min_lr=0.0001)
     savepoint = ModelCheckpoint(filepath="wavelet_model.hdf5", monitor='val_acc', save_best_only=True, mode='max')
 
-    clf = network(input_shape=(L*3,))
+    clf = network(input_shape=(3,))
 
     clf.fit(x_train, y_train, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE,
             shuffle=True, callbacks=[savepoint, reduce_lr], validation_data=(x_test, y_test))
@@ -112,4 +158,6 @@ if __name__ == "__main__":
     X, Y, x, y = prepare_data()
     print('Starting training')
     train(X, Y, x, y)
+
+    del [X, Y, x, y]
     print('finished')
